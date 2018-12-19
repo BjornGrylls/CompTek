90ea99ef860d182af579a84eb1408d41b1979e61
BjornGrylls <bjornskole@gmail.com>
2018-12-19 12:41:08 +0100

    Connected C# and webpage
    Webpage can send and receive messages and so can C# program

--- CompTek/Program.cs
+++ CompTek/Program.cs
@@ -1,12 +1,145 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using SuperWebSocket;
+using System;
 
-namespace CompTek {
+namespace ConsoleApp1 {
     class Program {
+        private static WebSocketServer wsServer;
         static void Main(string[] args) {
+            wsServer = new WebSocketServer();
+            int port = 1337;
+            wsServer.Setup(port);
+            wsServer.NewSessionConnected += WsServer_NewSessionConnected;
+            wsServer.NewMessageReceived += WsServer_NewMessageReceived;
+            wsServer.NewDataReceived += WsServer_NewDataReceived;
+            wsServer.SessionClosed += WsServer_SessionClosed;
+            wsServer.Start();
+            Console.WriteLine("Server is running on port " + port + ". Press ENTER to exit....");
+            Console.ReadKey();
+        }
+
+        private static void WsServer_SessionClosed(WebSocketSession session, SuperSocket.SocketBase.CloseReason value) {
+            Console.WriteLine("SessionClosed");
+        }
+
+        private static void WsServer_NewDataReceived(WebSocketSession session, byte[] value) {
+            Console.WriteLine("NewDataReceived");
+        }
+
+        private static void WsServer_NewMessageReceived(WebSocketSession session, string value) {
+            Console.WriteLine("NewMessageReceived: " + value);
+            if (value == "Hello server") {
+                session.Send("Hello client");
+            }
+        }
+
+        private static void WsServer_NewSessionConnected(WebSocketSession session) {
+            Console.WriteLine("NewSessionConnected");
         }
     }
 }
+
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Data.SqlClient;
+//using System.Net;
+//using System.IO;
+//using Microsoft.Web.WebSockets;
+//using System.Timers;
+
+//namespace CompTek {
+//    class Program {
+//        static void Main(string[] args) {
+//            Methods methods = new Methods();
+
+
+//            /*SqlConnection myConnection = new SqlConnection("server = localhost; user id = root; database = comptekbase" +
+
+//                                           "Trusted_Connection=yes;" +
+//                                           "Integrated security=true;" +
+//                                           "database=comptekbase;" +
+//                                           "connection timeout=15");
+
+//            myConnection.Open();*/
+
+
+//            HttpListener listener = new HttpListener();
+
+//            listener.Prefixes.Add("http://127.0.0.1:1337/");
+//            listener.Start();
+//            string path = methods.GetParent(".\\", 4).ToString();
+//            System.Diagnostics.Process.Start(path + "\\index.html");
+//            bool running = true;
+//            while (running) {
+//                Console.WriteLine("Waiting");
+//                HttpListenerContext context = listener.GetContext();
+//                string message = "Ian";
+//                context.Response.ContentLength64 = Encoding.UTF8.GetByteCount(message);
+//                context.Response.StatusCode = (int)HttpStatusCode.OK;
+//                using (Stream stream = context.Response.OutputStream) {
+//                    using (StreamWriter writer = new StreamWriter(stream)) {
+//                        writer.Write(message);
+//                    }
+
+//                }
+//            }
+
+//            Console.WriteLine("Message sent");
+//        }
+
+
+
+//    }
+
+//    public class MicrosoftWebSockets : WebSocketHandler {
+//        private static WebSocketCollection clients = new WebSocketCollection();
+//        private string name;
+
+//        public MicrosoftWebSockets() { }
+
+//        public override void OnOpen() {
+//            name = WebSocketContext.QueryString["chatname"];
+//            clients.Add(this);
+//            clients.Broadcast(name + " has connected.");
+
+//            SetTimer();
+//        }
+//        private static Timer aTimer;
+//        private static void SetTimer() {
+//            aTimer = new Timer(2000);
+
+//            aTimer.Elapsed += OnTimedEvent;
+//            aTimer.AutoReset = true;
+//            aTimer.Enabled = true;
+//        }
+//        private static void OnTimedEvent(object source, ElapsedEventArgs e) {
+//            clients.Broadcast("The Elapsed event" + e.SignalTime);
+//        }
+
+//        public override void OnMessage(string message) {
+//            clients.Broadcast("from server --" + message);
+//        }
+
+//        public override void OnClose() {
+//            clients.Remove(this);
+//            clients.Broadcast(string.Format(name + " has gone away."));
+
+//            aTimer.Stop();
+//            aTimer.Dispose();
+//        }
+//    }
+
+//    class Methods {
+//        public string GetParent(string directory, int times) {
+
+//            for (int i = 0; i < times; i++) {
+//                directory = Directory.GetParent(directory).ToString();
+//            }
+//            return directory;
+
+//        }
+//    }
+//}
--- scripts.js
+++ scripts.js
@@ -1,150 +1,58 @@
 $(document).ready(function() {
-    var url = "https://api.nasa.gov/planetary/apod?api_key=Nope"
-    var masterBlaster = $("#masterBlaster");
-    var tempPctScrolled = window.pageYOffset
-    var ged = $(".ged")
-    var darthFader = $(".darthFader")
-    var cloner = $("button.clone")
-
-    $.get(url, function(data) {
-        $("p").text(data.title) 
-    })
-
-    //BUTTONS
-    darthFader.click(function(){
-       $(this).siblings(ged).not("button").fadeOut()
-    })
-
-    cloner.click(function(){
-        $(this).parent().clone(true).addClass("chip").insertAfter($(this).parent())
-        ged = $(".ged")
-        darthFader = $(".darthFader")
-        cloner = $("button.clone")
-    })
-
-    masterBlaster.click(function(){
-        ged.fadeIn()
-        $.get(url, function(data) {
-            ged.attr("src",data.hdurl)
-        })
-/*
-        $.get(url, function(data) {
-            ged.eq(Math.floor((Math.random() * 3))).attr("src",data.hdurl)
-        })
-*/
-    })
-
-    $("#order66").click(function(){
-        $(".chip").remove()
-    })
-
-
-    //SCROLL
-    function amountscrolled(){
-        var winheight = $(window).height()
-        var docheight = $(document).height()
-        var scrollTop = $(window).scrollTop()
-        var trackLength = docheight - winheight
-        var pctScrolled = Math.floor(scrollTop/trackLength * 100)
-        $("img#bar").css("width",pctScrolled + "%");
-        //$("progress").val(pctScrolled);
-        if (tempPctScrolled < pctScrolled) {
-            $("#menu").finish().css("top","-20px")
-        } else if (tempPctScrolled > pctScrolled) {
-            $("#menu").finish().css("top","0px")
-
-        }
-        tempPctScrolled = pctScrolled
-    }
-     
-    $(window).on("scroll", function(){
-        amountscrolled()
-    })
-
-
-    //SNAKE
-    var direction
-    var canvas = document.getElementById("snakeCanvas")
-    var ctx = canvas.getContext("2d")
-    var x = canvas.width/2-50/2
-    var y = canvas.height/2
-    var xfood
-    var yfood
-
-    do {
-        xfood = Math.floor((Math.random() * canvas.width-10))
-        yfood = Math.floor((Math.random() * canvas.height-10))    
-    } while (Touching());
-
-    function CalculateFood() {
-        while (Touching()) {
-            xfood = Math.floor((Math.random() * canvas.width-10))
-            yfood = Math.floor((Math.random() * canvas.height-10))
-        } 
-    }
-
-    function Touching () {
-        var touching = false
-
-        if ((x > xfood-50 && x < xfood+20) && (y > yfood-50 && y < yfood+20)) {
-            touching = true
-        }
-
-        return touching
-
-    }
-
-    $(document).keydown(function(e) {
-        switch(e.which) {
-            case 65: // left
-                direction = (direction != 3 ? 1 : 3)
-            break;
-            case 87: // up
-            direction = (direction != 4 ? 2 : 4)
-            break;
-            case 68: // right
-            direction = (direction != 1 ? 3 : 1)
-            break;
-            case 83: // down
-            direction = (direction != 2 ? 4 : 2)
-            break;
-            default: return; // exit this handler for other keys
-        }
-    });
-
-    var speed = 10
-    setInterval(function(){
-        switch(direction) {
-            case 1:
-                x -= speed
-            break;
-            case 2:
-                y -= speed
-            break;
-            case 3:
-                x += speed
-            break;
-            case 4:
-                y += speed
-            break;
-            default:
-        }
-        x = (x < 0-40 ? 390 : x)
-        x = (x > 390 ? 0-40 : x)
-        y = (y < 0-40 ? 390 : y)
-        y = (y > 390 ? 0-40 : y)
-
-        CalculateFood()
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.fillStyle = "#0000FF"
-        ctx.fillRect(x,y,50,50)
-        ctx.fillStyle = "#FF0000"
-        ctx.fillRect(xfood,yfood,20,20)
-
-    }, 100);
-
-    if (x==1) {
+    
+    url = "ws://localhost:1337";
+    w = new WebSocket(url);
+    w.onopen = function() {
+        console.log("Open socket on" + url);
         
+        w.send("Hello server")
     }
 
+    w.onmessage = function(e) {
+        console.log(e.data.toString())
+    }
+    w.onclose = function(e) {
+        log("closed");
+    }
+    w.onerror = function(e) {
+        log("error");
+    }
+
+    window.onload = function() {
+        document.getElementById("login").onclick = function() {
+            console.log($("#username").val());
+            w.send(document.getElementById("username").value + "," + document.getElementById("password").value);
+        }
+    }
+
+
+    //t = tcpclient('127.0.0.1', 1337, 'Timeout', 30)
+/*
+    function WebSocketTest() {
+        
+        // Let us open a web socket
+        var ws = new WebSocket("ws://127.0.0.1:1337/echo");
+            
+        ws.onopen = function() {
+            
+            // Web Socket is connected, send data using send()
+            ws.send("Message to send");
+            alert("Message is sent...");
+        };
+        
+        ws.onmessage = function (evt) { 
+            var received_msg = evt.data;
+            $("p").text(received_msg)
+            alert("Message is received...");
+        };
+        
+        ws.onclose = function() { 
+            
+            // websocket is closed.
+            alert("Connection is closed..."); 
+        }
+    }
+
+    WebSocketTest() 
+*/
 })
